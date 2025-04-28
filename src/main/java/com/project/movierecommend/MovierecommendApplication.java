ebbe1b0fb4977ea77d5c364db6e3dd0b19e34578
김재령 <wofud0321@naver.com>
2025-04-29 07:50:25 +0900

    commit

---
+++ src/main/java/com/project/movierecommend/MovierecommendApplication.java
@@ -0,0 +1,13 @@
+package com.project.movierecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecommendApplication.class, args);
+	}
+
+}
