92c7371ec581d3bcb3a6526245a3489ba8764858
김재령 <wofud0321@naver.com>
2025-04-29 22:04:40 +0900

    sql 파일 생성

---
+++ src/main/sql/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE movies (
+                        movie_id BIGINT PRIMARY KEY,
+                        title VARCHAR(255),
+                        genres VARCHAR(255)
+);
+
+CREATE TABLE ratings (
+                         rating_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         user_id BIGINT,
+                         movie_id BIGINT,
+                         rating FLOAT,
+                         timestamp BIGINT
+);
