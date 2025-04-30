75b17ec5f19a2b0eded7702edbc48101da11fa99
김재령 <wofud0321@naver.com>
2025-04-30 14:34:11 +0900

    특정 Topic으로 전달하는 Producer 구현

---
+++ src/main/java/com/project/movierecommend/service/KafkaProducerService.java
@@ -0,0 +1,26 @@
+package com.project.movierecommend.service;
+
+import com.project.movierecommend.domain.Movie;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducerService {
+    /*
+        Movie 객체 정보를 특정 토픽으로 전송하는 프로듀서(Producer) 역할을 하는 부분
+        Key를 사용하면 같은 Key를 가진 메시지가 같은 파티션으로 전송되도록 보장
+        결국 Movie 객체를 "movie-info"라는 이름의 Kafka 토픽으로 비동기적으로 전송하는 기능
+     */
+    private final KafkaTemplate<String, Movie> kafkaTemplate;
+
+    private static final String TOPIC = "movie-info";
+
+    public void sendMovie(Movie movie) {
+        log.info("Producing movie to Kafka: {}", movie.getTitle());
+        kafkaTemplate.send(TOPIC, movie);
+    }
+}
