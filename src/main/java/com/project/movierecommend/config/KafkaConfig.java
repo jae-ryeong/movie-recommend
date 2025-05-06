88e4650468d7be3c586d44038e12248932914abf
김재령 <wofud0321@naver.com>
2025-05-06 14:30:36 +0900

    KafkaConfig 파일 수정

--- src/main/java/com/project/movierecommend/config/KafkaConfig.java
+++ src/main/java/com/project/movierecommend/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.project.movierecommend.config;
 
 import com.project.movierecommend.domain.MovieDocument;
+import com.project.movierecommend.domain.MovieEntity;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +19,7 @@ public class KafkaConfig {
 
     // ProducerFactory는 Kafka Producer 인스턴스를 생성하는 역할
     @Bean
-    public ProducerFactory<String, MovieDocument> producerFactory() {
+    public ProducerFactory<String, MovieEntity> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
 
         // Kafka 브로커의 연결 정보를 설정하는 키, Kafka 브로커가 로컬 머신의 9092 포트에서 실행되고 있음을 지정
@@ -32,7 +33,7 @@ public class KafkaConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, MovieDocument> kafkaTemplate() {
+    public KafkaTemplate<String, MovieEntity> kafkaTemplate() {
         // 보통 <String, String>이지만 <String, Movie>로 타입을 지정해주면서 타입 안정성이 높아졌다.
         return new KafkaTemplate<>(producerFactory());
     }
